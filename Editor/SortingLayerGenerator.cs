2608017648b3cc907f8d4858c8c8f309237994dc
Christiaan Bloemendaal <github@tnrd.net>
2020-07-11 15:21:40 +0200

    Added project

---
+++ Editor/SortingLayerGenerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TNRD.CodeGeneration.Layers
+{
+    public class SortingLayerGenerator : ICodeGenerator
+    {
+        [MenuItem("TNRD/Code Generation/Sorting Layers")]
+        private static void Execute()
+        {
+            var generator = new SortingLayerGenerator();
+            generator.Generate();
+        }
+
+        public void Generate()
+        {
+            var layers = SortingLayer.layers;
+
+            var generator = new Generator();
+            var @class = new Class("SortingLayers");
+
+            for (int i = 0; i < layers.Length; i++)
+            {
+                var layer = layers[i];
+                var layerName = Utilities.GetScreamName(layer.name);
+                var layerIdName = layerName + "_ID";
+
+                @class.AddField(
+                    new Field(layerName, layer.name, typeof(string))
+                    {
+                        IsConst = true,
+                    });
+
+                @class.AddField(
+                    new Field(layerIdName, layer.id, typeof(int))
+                    {
+                        IsConst = true,
+                    });
+            }
+
+            generator.AddClass(@class);
+            generator.SaveToFile(Application.dataPath + "/Generated/SortingLayers.cs");
+
+            AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ Editor/TagCodeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace TNRD.CodeGeneration.Tags
+{
+    public class TagCodeGenerator : ICodeGenerator
+    {
+        [MenuItem("TNRD/Code Generation/Tags")]
+        private static void Execute()
+        {
+            var generator = new TagCodeGenerator();
+            generator.Generate();
+        }
+
+        public void Generate()
+        {
+            var tags = InternalEditorUtility.tags;
+
+            var generator = new Generator();
+            var tagsClass = new Class("Tags");
+
+            foreach (var tag in tags)
+            {
+                tagsClass.AddField(
+                    new Field(Utilities.GetScreamName(tag), tag, typeof(string))
+                {
+                    IsConst = true,
+                });
+            }
+
+            generator.AddClass(tagsClass);
+            generator.SaveToFile(Application.dataPath + "/Generated/Tags.cs");
+
+            AssetDatabase.Refresh();
+        }
+    }
+}
